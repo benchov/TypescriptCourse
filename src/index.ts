fe9ccd19250e7126aabca1e74fa208bfc43dc08a
Viktor Mihályi <viktor.mihalyi@yahoo.com>
2020-11-14 22:40:59 +0100

    feat(User.ts): add and test new get method

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,6 @@
-console.log('Typescript Project')
+import { User } from "./models/User";
+
+//TEST LINE
+const user = new User({ name: "Lucsisson Gelufrik", age: 14 });
+console.log(user.get("name"));
+console.log(user.get("age"));
