c54aa297fe78a32c88f5a900ce8dc91b60a6efe7
Viktor Mihályi <viktor.mihalyi@yahoo.com>
2020-11-14 22:57:18 +0100

    feat(User.ts): add and test set method

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,8 @@ import { User } from "./models/User";
 
 //TEST LINE
 const user = new User({ name: "Lucsisson Gelufrik", age: 14 });
+
+user.set({ name: "Daniel Silverado", age: 56 });
+
 console.log(user.get("name"));
 console.log(user.get("age"));
