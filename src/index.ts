93e23c3e4f707d34f6af4f0f8b9aeca94778b19e
Viktor Mihályi <viktor.mihalyi@yahoo.com>
2020-11-15 14:19:11 +0100

    feat(User.ts): add and test new trigger method

--- src/index.ts
+++ src/index.ts
@@ -3,13 +3,14 @@ import { User } from "./models/User";
 //TEST LINE
 const user = new User({ name: "Lucsisson Gelufrik", age: 14 });
 
-// user.set({ name: "Daniel Silverado", age: 56 });
+user.set({ name: "Daniel Silverado", age: 56 });
 
-// console.log(user.get("name"));
-// console.log(user.get("age"));
+console.log(user.get("name"));
+console.log(user.get("age"));
 
-user.on("change", () => {});
-user.on("change", () => {});
-user.on("test", () => {});
+user.on("test", () => {
+  console.log("test event was triggered!");
+});
 
 console.log(user);
+user.trigger("test");
