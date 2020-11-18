883e0a43cf1e3efe0c00564d606b10153d9c60c8
Viktor Mihályi <viktor.mihalyi@yahoo.com>
2020-11-18 14:14:33 +0100

    refactor(Eventing.ts): move eventing logic to separate file

--- src/models/User.ts
+++ src/models/User.ts
@@ -4,13 +4,7 @@ interface UserProps {
   age?: number;
   id?: number;
 }
-type Callback = () => void;
 export class User {
-  // IMPORTANT!
-  // in order to on func does not crash with this.events = undefined the
-  // events property was initailazed with default value
-  events: { [key: string]: Callback[] } = {};
-
   constructor(private data: UserProps) {}
 
   get(propName: string): number | string {
@@ -19,24 +13,6 @@ export class User {
 
   set(update: UserProps): void {
     Object.assign(this.data, update);
-  }
-
-  on(eventName: string, callback: Callback): void {
-    const handlers = this.events[eventName] || [];
-    handlers.push(callback);
-    this.events[eventName] = handlers;
-  }
-
-  trigger(eventName: string): void {
-    const handlers = this.events[eventName];
-
-    if (!handlers || handlers.length === 0) {
-      return;
-    }
-
-    handlers.forEach((callback) => {
-      callback();
-    });
   }
 
   fetch(): void {
