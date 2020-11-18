22c6dc2f51188a6db9f6e3f2edbca9ffb7fddf21
Viktor Mihályi <viktor.mihalyi@yahoo.com>
2020-11-18 13:49:22 +0100

    feat(User.ts): Add save and delete functionallity to User model

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,7 @@
 import { User } from "./models/User";
 
-const user = new User({ id: 1 });
+const user = new User({ id: 3 });
+// user.delete();
 
-user.fetch();
-
-setTimeout(() => {
-  console.log("user ", user);
-}, 4000);
+user.set({ name: "Norm Fac", age: 43 });
+user.save();
