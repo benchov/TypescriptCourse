045a8d492f666893ccfaebb00b3b098c5afafd97
Viktor Mihályi <viktor.mihalyi@yahoo.com>
2020-11-22 16:40:32 +0100

    feat(Attributes.ts): Create new class and put get and set to it

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,3 +1,4 @@
+import { Attributes } from "./Attributes";
 import { Eventing } from "./Eventing";
 import { Sync } from "./Sync";
 export interface UserProps {
@@ -10,13 +11,5 @@ const rootUrl = "http://localhost:3000/users";
 export class User {
   public events: Eventing = new Eventing();
   public sync: Sync<UserProps> = new Sync<UserProps>(rootUrl);
-  constructor(private data: UserProps) {}
-
-  get(propName: string): number | string {
-    return this.data[propName];
-  }
-
-  set(update: UserProps): void {
-    Object.assign(this.data, update);
-  }
+  public attributes: Attributes<UserProps> = new Attributes<UserProps>();
 }
