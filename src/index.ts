bd237e047c62f4eba0f48166ef47b83746653945
Viktor Mihályi <viktor.mihalyi@yahoo.com>
2020-11-29 17:23:14 +0100

    fix(models/): Turn normal function into arrow function
    In order to fix binding issue which comes from normal function declaration this commit changed it.

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,9 @@ import { User } from "./models/User";
 const user = new User({ name: "Francis" });
 
 console.log(user.get("name"));
+
+user.on("change", () => {
+  console.log("The was changed");
+});
+
+user.trigger("change");
