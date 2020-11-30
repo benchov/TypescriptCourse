c26922a1fce579d4ee0f6707461c1eafdb68d803
Viktor Mihályi <viktor.mihalyi@yahoo.com>
2020-11-30 10:10:33 +0100

    feat(User.ts): Add saving feature with error handling and event triggering

--- src/models/User.ts
+++ src/models/User.ts
@@ -47,4 +47,15 @@ export class User {
       this.set(response.data);
     });
   };
+
+  save = (): void => {
+    this.sync
+      .save(this.attributes.getAll)
+      .then((res: AxiosResponse) => {
+        this.trigger("save");
+      })
+      .catch(() => {
+        this.trigger("error");
+      });
+  };
 }
