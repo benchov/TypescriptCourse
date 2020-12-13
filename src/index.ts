336b07b9aaf17502d2dfc14d123ef9f3ae482ff8
Viktor Mihályi <viktor.mihalyi@yahoo.com>
2020-12-13 15:15:34 +0100

    feat(Collection.ts): Parsing user collection

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
-import axios, { AxiosResponse } from "axios";
+import { Collection } from "./models/Collection";
 
-const rootUrl = "http://localhost:3000/users";
+const collection = new Collection("http://localhost:3000/users");
+collection.fetch();
 
-axios.get(rootUrl).then((response: AxiosResponse) => {
-  console.log(response.data);
+collection.on("change", () => {
+  console.log(collection);
 });
