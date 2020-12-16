5335df81f3d0ddc68be9e43c2e1c55160efe3563
Viktor Mihályi <viktor.mihalyi@yahoo.com>
2020-12-16 08:59:31 +0100

    feat(UserForm.ts): Binding event handlers and represent model data

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
 import { UserForm } from "./views/UserForm";
+import { User } from "./models/User";
 
-const userForm = new UserForm(document.getElementById("root"));
+const user = User.buildUser({ name: "Marci", age: 11 });
+
+const userForm = new UserForm(document.getElementById("root"), user);
 
 userForm.render();
--- src/views/UserForm.ts
+++ src/views/UserForm.ts
@@ -1,10 +1,17 @@
+import { User } from "../models/User";
+
 export class UserForm {
-  constructor(public parent: Element) {}
+  constructor(public parent: Element, public model: User) {}
 
   onEventHandler(): { [key: string]: () => void } {
     return {
       "click:button": this.onButtonClick,
+      "mouseenter:h1": this.onHoverHeader,
     };
+  }
+
+  onHoverHeader(): void {
+    console.log("H1 Hovered");
   }
 
   onButtonClick(): void {
@@ -15,15 +22,31 @@ export class UserForm {
     return `
         <div>
             <h1>USER FORM</h1>
+            <div>User Name: ${this.model.get("name")}</div>
+            <div>User Age: ${this.model.get("age")}</div>
             <input />
+            <button>Click Me</button>
         </div>
      `;
+  }
+
+  bindEvents(fragment: DocumentFragment): void {
+    const eventsMap = this.onEventHandler();
+
+    for (const eventKey in eventsMap) {
+      const [eventName, selector] = eventKey.split(":");
+
+      fragment.querySelectorAll(selector).forEach((element) => {
+        element.addEventListener(eventName, eventsMap[eventKey]);
+      });
+    }
   }
 
   render(): void {
     const templateElement = document.createElement("template");
     templateElement.innerHTML = this.template();
 
+    this.bindEvents(templateElement.content);
     this.parent.append(templateElement.content);
   }
 }
