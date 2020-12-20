42721ef0f6ce885fe26b5fcf6c43a1b802bfd8c2
Viktor Mihályi <viktor.mihalyi@yahoo.com>
2020-12-20 14:25:21 +0100

    feat(UserForm.ts): Save changes to db

--- src/views/UserForm.ts
+++ src/views/UserForm.ts
@@ -8,13 +8,19 @@ export class UserForm extends View<User, UserProps> {
       "mouseenter:h1": this.onHoverHeader,
       "mouseleave:h1": this.onLeaveHeader,
       "click:.set-age": this.onSetAge,
+      "click:.save-form": this.onSave,
     };
   }
+
+  onSave = (): void => {
+    this.model.save();
+  };
 
   onSetAge = (): void => {
     console.log("Set Age was pressed");
     this.model.setRandomAge();
   };
+
   onHoverHeader(): void {
     console.log("H1 Hovered");
   }
@@ -34,12 +40,10 @@ export class UserForm extends View<User, UserProps> {
   template(): string {
     return `
         <div>
-            <h1>USER FORM</h1>
-            <div>User Name: ${this.model.get("name")}</div>
-            <div>User Age: ${this.model.get("age")}</div>
-            <input />
+            <input placeholder="${this.model.get("name")}"/>
             <button class='change-name'>Change Name</button>
             <button class='set-age'>Set Random Age</button>
+            <button class='save-form'>Save</button>
         </div>
      `;
   }
