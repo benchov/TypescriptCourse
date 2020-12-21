0cff2afef3fb14a70a82c619024d4fa6a1579a5c
Viktor Mihályi <viktor.mihalyi@yahoo.com>
2020-12-21 22:44:55 +0100

    draft feat(UserEdit.ts): bind view nesting

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,15 @@
-import { UserForm } from "./views/UserForm";
+import { UserEdit } from "./views/UserEdit";
 import { User } from "./models/User";
 
 const user = User.buildUser({ name: "Marci", age: 11 });
 
-const userForm = new UserForm(document.getElementById("root"), user);
+const root = document.getElementById("root");
+console.log("999 ", root);
+if (root) {
+  const userEdit = new UserEdit(root, user);
+  userEdit.render();
 
-userForm.render();
+  console.log("UserEdit ", userEdit);
+} else {
+  throw new Error("Something goes wrong!");
+}
