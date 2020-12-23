39276eb5cf3c5a86bd48d9a086e9dd11da835214
Viktor Mihályi <viktor.mihalyi@yahoo.com>
2020-12-23 13:22:17 +0100

    feat(index.ts): Basic server setup

--- express_ts_integration/src/index.ts
+++ express_ts_integration/src/index.ts
@@ -1,1 +1,17 @@
-console.log("Welcome");
+import express, { Request, Response } from "express";
+
+const app = express();
+
+app.get("/", (req: Request, res: Response) => {
+  res.send(
+    `
+    <div>
+        <h1>Hello Stranger</h1>
+    </div>
+    `
+  );
+});
+
+app.listen(3000, () => {
+  console.log("Server running on 3000");
+});
