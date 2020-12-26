9919399592b4e75499e4c76a240e592f5264b620
Viktor Mihályi <viktor.mihalyi@yahoo.com>
2020-12-26 14:59:16 +0100

    feat(loginRoutes.ts):  basic implmentation of login method

--- express_ts_integration/src/routes/loginRoutes.ts
+++ express_ts_integration/src/routes/loginRoutes.ts
@@ -1,9 +1,29 @@
-import { Router } from "express";
+import { Router, Request, Response } from "express";
 
 const router = Router();
 
-router.get("/", (req, res) => {
-  res.send("Hello Bello");
+router.get("/login", (req: Request, res: Response) => {
+  res.send(`
+    <form method='POST'>
+        <div>
+        <label>Email</label>
+        <input name="email" />
+        </div>
+        <div>
+        <label>Password</label>
+        <input name="password" type="password" />
+        </div>
+        <button>Submit</button>
+    </form>
+    
+  `);
+});
+
+router.post("/login", (req: Request, res: Response) => {
+  console.log("req.body", req.body);
+  const { email, password } = req.body;
+
+  res.send(email + password);
 });
 
 export { router };
